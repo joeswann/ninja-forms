7416ca68021f51f1e0282ea65e1cbec7467fdd6e
klhall1987@gmail.com <klhall1987@gmail.com>
2016-07-28 10:41:28 -0400

    Finished first pass on PHP localization. Moving into the JS localization now.

--- includes/Templates/ui-nf-toggle-drawer.html.php
+++ includes/Templates/ui-nf-toggle-drawer.html.php
@@ -1,3 +1,3 @@
 <a class="nf-toggle-drawer">
-    <span class="dashicons dashicons-admin-collapse"></span><span class="nf-expand-off">Full screen</span><span class="nf-expand-on">Half screen</span>
+    <span class="dashicons dashicons-admin-collapse"></span><span class="nf-expand-off">Full screen</span><span class="nf-expand-on"><?php _e( 'Half screen', 'ninja-forms' ); ?></span>
 </a>
